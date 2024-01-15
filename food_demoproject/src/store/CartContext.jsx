cbbc002422929d6dd70bb6febb514e6060ae639a
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-01-15 18:08:09 +0900

    food demoproject v2

--- food_demoproject/src/store/CartContext.jsx
+++ food_demoproject/src/store/CartContext.jsx
@@ -1,5 +1,70 @@
-import React from 'react'
-
+import React, { useReducer, useState } from 'react'
+import { createContext } from 'react'
 const CartContext= createContext({
-    
+    items:[],
+    addItem: (item) =>{},
+    removeItem: (id) => {}
 })
+
+function cartReducer(state, action){
+    //장바구니 추가
+    if(action.type==='ADD_ITEM'){
+        const existingCartItemIndex=state.items.findIndex((item) => item.id===action.item.id);
+        const updatedItems = [...state.items];
+
+        if(existingCartItemIndex > -1){
+            const existingItem = state.items[existingCartItemIndex]
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            }
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        else{
+            updatedItems.push({...action.item, quantity: 1})
+        }
+        return {...state,items:updatedItems}
+    }
+    //장바구니 삭제
+    if(action.type==='REMOVE_ITEM'){
+        const existingCartItemIndex=state.items.findIndex(
+            (item) => item.id===action.item.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedItems = [...state.items];
+
+        if(existingCartItem.quantity ===1){
+            updatedItems.splice(existingCartItemIndex,1);
+        }
+        else{
+            const updatedItem ={...existingCartItem, quantity: existingCartItem.quantity -1}
+            updatedItem[existingCartItemIndex] = updatedItem
+        }
+        return {...state, items:updatedItems}
+    }
+
+    return state
+}
+
+export function CartContextProvider ({children}){
+    const [cart,dispatchCartAction] = useReducer(cartReducer,{items:[]});
+   
+    const cartContext= {
+        items:cart.items
+    }
+   
+    function addItem(item){
+        dispatchCartAction({
+            
+        })
+    }
+    function removeItem (id){
+
+    }
+   return <CartContext.Provider value={cartContext}>
+        {children}
+    </CartContext.Provider>
+}
+
+export default CartContext;
