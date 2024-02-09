cf57ab7d01509218590d5b118a0185b07d03b811
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-02-10 04:09:16 +0900

    로그인 토큰 관리하기

--- login_certified/frontend/src/App.js
+++ login_certified/frontend/src/App.js
@@ -13,13 +13,16 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication';
-
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
+import { checkAuthLoader, tokenLoader } from './util/auth';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -45,6 +48,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader
               },
             ],
           },
@@ -52,18 +56,24 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader
           },
         ],
       },
       {
-        path:'auth',
-        element: <AuthenticationPage/>
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: 'newsletter',
         element: <NewsletterPage />,
         action: newsletterAction,
       },
+      {
+        path: 'logout',
+        action: logoutAction,
+      }
     ],
   },
 ]);
