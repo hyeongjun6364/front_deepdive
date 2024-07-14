e4e52009c65b01050429aee656711d9ed333734b
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-14 16:50:15 +0900

    implement error page

--- nextjsex/foodieApp/lib/meal.js
+++ nextjsex/foodieApp/lib/meal.js
@@ -6,6 +6,8 @@ const db=sql('meals.db');
 export async function getMeals(){
     //일반적으로 추가지연하는 함수를 넣진 않지만 로딩관련 실습을 해야하기에 적용함.
     await new Promise((resolve)=>setTimeout(resolve, 5000))
+
+    //throw new Error('Loading meals failed');
     //모든요소를 다 선택하고 데이터 불러오기
     return db.prepare('SELECT * FROM meals').all();
 }
