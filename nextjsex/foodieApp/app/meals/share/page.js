96db04e3d4457b752cb275d2bf27f1e62ebd8a9c
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-14 14:43:23 +0900

    meals컴포넌트 생성 및 데이터 규격 불분명한 이미지 출력

--- nextjsex/foodieApp/app/meals/share/page.js
+++ nextjsex/foodieApp/app/meals/share/page.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import classes from './page.module.css'
 function page() {
   return (
-    <div>page</div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+      </header> 
+      <main className={classes.main}>
+
+      </main>
+    </>
+   
   )
 }
 
