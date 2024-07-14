0a53c438d7d7cd05c83c44bf7ae5ee7050c42989
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-14 16:07:47 +0900

    implement loading component

--- nextjsex/foodieApp/lib/meal.js
+++ nextjsex/foodieApp/lib/meal.js
@@ -5,7 +5,7 @@ const db=sql('meals.db');
 //async를 활용해 promise를 반환시키기
 export async function getMeals(){
     //일반적으로 추가지연하는 함수를 넣진 않지만 로딩관련 실습을 해야하기에 적용함.
-    await new Promise((resolve)=>setTimeout(resolve, 2000))
+    await new Promise((resolve)=>setTimeout(resolve, 5000))
     //모든요소를 다 선택하고 데이터 불러오기
     return db.prepare('SELECT * FROM meals').all();
 }
