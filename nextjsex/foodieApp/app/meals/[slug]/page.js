d630a7997439cb9decc0c34d9821813db41f7b96
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-15 00:26:53 +0900

    meals 세부내용 렌더링 및 개선

--- nextjsex/foodieApp/app/meals/[slug]/page.js
+++ nextjsex/foodieApp/app/meals/[slug]/page.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import classes from './page.module.css';
 import Image from 'next/image';
+import { getMeal } from '@/lib/meal';
 
-function MealDetailPage() {
+async function MealDetailPage({params}) {
+  const meal = await getMeal(params.slug)
+  meal.instructions = meal.instructions.replace(/\n/g,'<br/>')
+  console.log(meal)
   return (
    <>
     <header className={classes.header}>
       <div className={classes.image}>
-        <Image fill/>
+        <Image src={meal.image} alt={meal.title} fill/>
       </div>
       <div className={classes.headerText}>
-        <h1>TITLE</h1>
+        <h1>{meal.title}</h1>
         <p className={classes.creator}>
-          by <a href={`mailto:${'EMAIL'}`}>NAME</a></p>
-          <p className={classes.summary}>SUMMARY</p>
+          by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+          <p className={classes.summary}>{meal.summary}</p>
       </div>
     </header>
     <main>
+      {/* 크로스 사이트 스크립트공격에 노출되기 때문에 dangerouslySetInnerHTML={{
+        __html:'...'을 작성함 */}
       <p className={classes.instructions} dangerouslySetInnerHTML={{
-        __html:'...',
+        __html:meal.instructions,
       }}></p>
     </main>
    </>
