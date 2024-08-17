443c07bdfc93c98b699cace1681bb0d3985165c3
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-08-17 16:28:44 +0900

    데이터베이스에 데이터 저장

--- nextjsex/foodieApp/lib/action.js
+++ nextjsex/foodieApp/lib/action.js
@@ -1,6 +1,9 @@
 //서버에서만 실행되는 함수이다. async를 무조건 붙여줘야한다.
 'use server';
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meal";
+
 export async function shareMeal(formData){
     //form 객체 만들기
     const meal ={
@@ -12,6 +15,7 @@ export async function shareMeal(formData){
       creator: formData.get('name'),
       creator_email: formData.get('email')
     }
-
-    console.log(meal)
+    await saveMeal(meal)
+    redirect('/meals')
+    //console.log(meal)
   }
--- nextjsex/foodieApp/lib/meal.js
+++ nextjsex/foodieApp/lib/meal.js
@@ -1,3 +1,6 @@
+//파일 시스템 api
+import fs from 'node:fs'
+
 import sql from 'better-sqlite3';
 import slugify from 'slugify';
 import xss from 'xss';
@@ -18,7 +21,35 @@ export function getMeal(slug){
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
 
-export function saveMeal(meal){
-    meal.slug = slugify(meal.title,{lower:ture});// 모든문자를 소문자로 전달
+export async function saveMeal(meal){
+    meal.slug = slugify(meal.title,{lower:true});// 모든문자를 소문자로 전달
     meal.instructions = xss(meal.instructions);
+
+    const extension = meal.image.name.split('.').pop(); //확장자 가져오기
+    const fileName = `${meal.slug}.${extension}`; // 파일명 짓고
+
+    const stream =  fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error){
+            throw new Error('Saving image failed!');
+        }
+    });
+    meal.image = `/images/${fileName}` // 모든 이미지에 관한 요청은 public으로 자동으로 요청되기에 public을 경로에서 빼기
+
+    db.prepare(`
+        INSERT INTO meals
+            (title, summary, instructions, creator, creator_email, image, slug)
+        VALUES ( 
+            @title,
+            @summary,
+            @instructions,
+            @creator,
+            @creator_email,
+            @image,
+            @slug
+            )
+    `).run(meal);//value와 속성의 순서가 같아야한다. 각 필드에 맞는 데이터가 자동 추출됨.
+
 }
